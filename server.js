b3b87d49681b3f153609c9227efce33184c5fccf
annelinderaadsheer <144004885+annelinderaadsheer@users.noreply.github.com>
2024-04-19 10:11:13 +0200

    Refactored
    Refactored

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
-console.log('Hier komt je server voor Sprint 10. Gebruik uit Sprint 9 alleen de code die je mee wilt nemen.')
+// console.log('Hier komt je server')
+
+// import express
+// import helpers < fetch ophalen
+
+
+// express aanmaken
+// public folder
+// post data mogelijk maken
+// views
+
+
+// route op /
+// https://redpers.nl/wp-json/wp/v2/posts
+// parsen naar index.ejs
+
+// detai route op /article/:slug
+// data ophalen van: https://redpers.nl/wp-json/wp/v2/posts/?slug-red-pers-zoekt
+// parcen nar article.ejs
+
+// start express op 
