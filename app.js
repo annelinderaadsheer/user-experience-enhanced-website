391b568af362a9b488a91a92a8c9ab167206cef7
annelinderaadsheer <144004885+annelinderaadsheer@users.noreply.github.com>
2024-04-20 12:33:17 +0200

    CSS
    Css aangepast van generiek naar specifiek

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 /*** Express setup & start ***/
-// Importeer de zelfgemaakte functie fetchJson uit de ./helpers map
-import fetchJson from "./helpers/fetch-json.js";
 
 // Importeer het npm pakket express uit de node_modules map
 import express from "express";
 
+// Importeer de zelfgemaakte functie fetchJson uit de ./helpers map
+import fetchJson from "./helpers/fetch-json.js";
+
+// Importeer slugify voor leesbare URLs met slug
+// import slugify from "slugify";
+
+// Declare de base URL van de directus API
+const baseUrl = "https://fdnd-agency.directus.app";
+
 // Maak een nieuwe express app aan
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Stel ejs in als template engine
 app.set("view engine", "ejs");
@@ -17,35 +26,40 @@ app.set("views", "./views");
 // Gebruik de map 'public' voor statische resources, zoals stylesheets, afbeeldingen en client-side JavaScript
 app.use(express.static("public"));
 
-//Zorg dat werken met request data makkelijker wordt
+// Fetch de data van de API
+
+// Data ophalen van de API
+
+
+// Zorg dat werken met request data makkelijker wordt
 app.use(express.urlencoded({ extended: true }));
 
-//Als iemand mijn route pagina bezoekt, dan rendert de homepage
+// Als iemand mijn route pagina bezoekt, dan rendert de homepage
 app.get("/", function (request, response) {
   response.render("homepage");
 });
 
-//Contact pagina
+// Get-route voor Contact pagina
 app.get("/contact", function (request, response) {
   response.render("contact");
 });
 
-//About pagina
+// Get-route voor About pagina
 app.get("/about", function (request, response) {
   response.render("about");
 });
 
-//FAQ pagina
+// Get-route voor FAQ pagina
 app.get("/faq", function (request, response) {
   response.render("faq");
 });
 
-//Opdracht pagina
+// Get-route voor Opdracht pagina
 app.get("/opdracht", function (request, response) {
   response.render("opdracht");
 });
 
-//Vraag-aanbod pagina, eigen data inladen 
+// Get-route voor Vraag-aanbod pagina, eigen data inladen 
 app.get("/vraag-aanbod", function (request, response) {
   fetchJson("https://fdnd-agency.directus.app/items/dh_services").then(
     (apiData) => {
